aa6b8736c70331cf0c8549bd50be10e11ef5f21f
Nikolay Dimov <114500017+NikolayDimov@users.noreply.github.com>
2024-06-06 16:45:21 +0300

    constants - colors

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,8 +1,7 @@
 import { View, Text, Pressable, StyleSheet } from 'react-native';
+import Colors from '../constants/colors';
 
 function PrimaryButton({ children, onPress }) {
-
-
     return (
         <View style={style.buttonOuterContainer}>
             <Pressable
@@ -12,7 +11,7 @@ function PrimaryButton({ children, onPress }) {
                         : style.buttonInnerContainer
                 }
                 onPress={onPress}
-                android_ripple={{ color: "#640233" }}>
+                android_ripple={{ color: Colors.primary600 }}>
                 <Text style={style.buttonText}>{children}</Text>
             </Pressable>
         </View>
@@ -28,7 +27,7 @@ const style = StyleSheet.create({
         overflow: 'hidden'
     },
     buttonInnerContainer: {
-        backgroundColor: '#72063c',
+        backgroundColor: Colors.primary500,
         paddingVertical: 8,
         paddingHorizontal: 16,
         elevation: 2

571b1b29dc9413b04beae39e59c40f1c5be8b509
Nikolay Dimov <114500017+NikolayDimov@users.noreply.github.com>
2024-06-06 09:41:23 +0300

    App.js: background image style

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -30,10 +30,10 @@ const styles = StyleSheet.create({
     inutContainer: {
         justifyContent: 'center',
         alignItems: 'center',
-        marginTop: 50,
+        marginTop: 100,
         marginHorizontal: 24,
         padding: 16,
-        backgroundColor: '#4e0329',
+        backgroundColor: '#3b021f',
         borderRadius: 8,
         elevation: 4, // shadow android
         shadowColor: 'black', // ios android
